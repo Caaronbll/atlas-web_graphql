42d73fd2cbcae1eb63fbfdcb3ea359318d14e1d1
Caaronbll <aaronbll46@gmail.com>
2024-03-19 01:01:30 -0500

    tasks 0-3

--- GraphQL_API/server/app.js
+++ GraphQL_API/server/app.js
@@ -1,13 +1,20 @@
+// Initial server file
+
+// imports
 const express = require('express');
-const {graphqlHTTP} = require('express-graphql');
+const { graphqlHTTP }= require('express-graphql');
 const schema = require('./schema/schema');
 
+// Express App
 const app = express();
 
-app.use('/graphql',graphqlHTTP({
+// gQL endpoint
+app.use('/graphql', graphqlHTTP({
   schema,
   graphiql: true,
 }));
-app.listen(4000,()=>{
+
+// Express local route
+app.listen(2000,()=>{
   console.log('now listening for request on port 4000');
 });
